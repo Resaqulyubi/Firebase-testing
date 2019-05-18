32c795b6dc02f521409de7b86321e0e4b29a3e7f
resaqulyubi <akatsukiresa@gmail.com>
2019-05-18 12:21:25 +0700

    :wrench: add firebase storage

--- app/src/main/java/exercise/com/myapplication/MainActivity.java
+++ app/src/main/java/exercise/com/myapplication/MainActivity.java
@@ -1,20 +1,27 @@
 package exercise.com.myapplication;
 
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -22,19 +29,57 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 import exercise.com.myapplication.app.Config;
+import exercise.com.myapplication.model.NomorAntrianModel;
 import exercise.com.myapplication.util.NotificationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PICK_IMAGE_REQUEST = 123;
     private BroadcastReceiver mRegistrationBroadcastReceiver;
     private TextView txtRegId, txtMessage;
+    private Button btnstorage;
+    private Uri filePath;
+    //Firebase
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+
+                uploadImage();
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+//                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,34 +87,74 @@ public class MainActivity extends AppCompatActivity {
 
         txtRegId = findViewById(R.id.txt_reg_id);
         txtMessage = findViewById(R.id.txt_push_message);
+        btnstorage = findViewById(R.id.btnstorage);
+
+        //firebase storage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        btnstorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);  
+            }
+        });
 
         //----------> FIREBASE REAL TIME DATABASE
 
-//        FirebaseApp.initializeApp(this);
-//        // Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//
-//        DatabaseReference myRef = database.getReference("message");
-//
-//        myRef.setValue("{\"success\": {\"message\": \"Welcome to Awesome JSON Viewer.\",\"status_code\": 200}}");
-//
-//        DatabaseReference sadsa = database.getReference("1321");
+        FirebaseApp.initializeApp(this);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference myRef = database.getReference("4-5-2019/aaa");
+
+        NomorAntrianModel model =new NomorAntrianModel("11","11","11","11","11");
+        NomorAntrianModel mode2 =new NomorAntrianModel("22","22","22","22","22");
+
+        myRef.setValue(model);
+        myRef.setValue(mode2);
+
+//        DatabaseReference sadsa = database.getReference("message");
 //        sadsa.setValue("aaa, aaaa!");
-//        myRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
+
+
+        DatabaseReference myRefRead = database.getReference("4-5-2019");
+        myRefRead.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
 //                String value = dataSnapshot.getValue(String.class);
-//                Log.d("aaaa", "Value is: " + value);
-//            }
+//                JsonObject value = dataSnapshot.getValue(JsonObject.class);
+
+                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
+
+                long value=dataSnapshot.getChildrenCount();
+
+                Log.d("aaaa", "Value is: " + value);
+
+
+//                long value=dataSnapshot.getChildrenCount();
+//                Log.d(TAG,"no of children: "+value);
 //
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                // Failed to read value
-//                Log.w("aaaa", "Failed to read value.", error.toException());
-//            }
-//        });
+//                GenericTypeIndicator<List<TaskDes>> genericTypeIndicator =new GenericTypeIndicator<List<TaskDes>>(){};
+//
+//                List<TaskDes> taskDesList=dataSnapshot.getValue(genericTypeIndicator);
+//
+//                for(int i=0;i<taskDesList.size();i++){
+//                    Toast.makeText(MainActivity.this,"TaskTitle = "+taskDesList.get(i).getTaskTitle(),Toast.LENGTH_LONG).show();
+//                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("aaaa", "Failed to read value.", error.toException());
+            }
+        });
 
 
 
@@ -148,12 +233,53 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         // [END retrieve_current_token]
-
-
-
-
-
     }
+
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            final StorageReference ref = storageReference.child("images/"+ "12345");
+//            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                                          @Override
+                                                                          public void onSuccess(Uri uri) {
+                                                                              Uri downloadUrl = uri;
+                                                                              //Do what you want with the url
+                                                                              progressDialog.dismiss();
+                                                                              Toast.makeText(MainActivity.this, "Uploaded" +downloadUrl, Toast.LENGTH_SHORT).show();
+                                                                          }
+                                                                      });
+
+
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
 
     // Fetches reg id from shared preferences
     // and displays on the screen
---
+++ app/src/main/java/exercise/com/myapplication/model/NomorAntrianModel.java
@@ -0,0 +1,63 @@
+package exercise.com.myapplication.model;
+
+public class NomorAntrianModel  {
+
+    private String nik;
+    private String nomor;
+    private String nama;
+    private String poli;
+    private String waktu;
+
+
+    public NomorAntrianModel(String nik, String nomor, String nama, String poli, String waktu){
+        this. nik =nik;
+        this.nomor =nomor;
+        this.nama =nama;
+        this.poli =poli;
+        this.waktu= waktu;
+    }
+
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPoli() {
+        return poli;
+    }
+
+    public void setPoli(String poli) {
+        this.poli = poli;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(String waktu) {
+        this.waktu = waktu;
+    }
+
+
+
+}
